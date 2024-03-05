b4e4e05174ef96412dae1d7ad01a95362f337235
Nugget-Meister <tremainecamacho@gmail.com>
2024-03-04 21:33:19 -0500

    empty paths w/ routes for mvp pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+import Navbar from './components/NavBar/Navbar'
+import Map from './components/pages/Map/Map'
+import Profile from './components/pages/Profile/Profile'
+import SavedRoutes from './components/pages/SavedRoutes/SavedRoutes'
+import Splash from './components/pages/Splash/Splash'
 
 function App() {
 
   return (
     <>
+      <Router>
+        <Navbar />
+        <Route path={'/'} element={<Splash/>}/>
+        <Route path={'/map'} element={<Map/>}/>
+        <Route path={'/profile/:username'} element={<Profile/>}/>
+        <Route path={'/saved'} element={<SavedRoutes/>}/>
+      </Router>
     </>
   )
 }
---
+++ src/components/pages/SavedRoutes/SavedRoutes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SavedRoutes = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default SavedRoutes;
