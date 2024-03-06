69f3c73799f9c9cbd85bac0775fad75cea3b524f
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2024-03-05 22:23:29 -0500

    Adds component files

---
+++ src/components/pages/Map/mapComponents/inputCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function inputGroupCard() {
+  return (
+    <div>inputGroupCard</div>
+  )
+}
---
+++ src/components/pages/Map/mapComponents/timerBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function timerBar() {
+  return (
+    <div>timerBar</div>
+  )
+}
