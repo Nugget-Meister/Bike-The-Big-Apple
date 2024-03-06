62d7b7a4367ca7bfa9aceab152efac207f95c1f7
Nugget-Meister <tremainecamacho@gmail.com>
2024-03-05 20:46:14 -0500

    init map.jsx and create empty mapwidget.jsx for containter

--- src/components/pages/Map/Map.jsx
+++ src/components/pages/Map/Map.jsx
@@ -1,10 +1,20 @@
+import './Map.css'
 import React from 'react';
+import MapWidget from './subcomponents/MapWidget';
+
+import { Loader } from "@googlemaps/js-api-loader"
 
 const Map = () => {
+
+    const key = import.meta.env.VITE_API_KEY;
+ 
+
+
+
     return (
-        <div>
-            
-        </div>
+      <>
+        <MapWidget/>
+      </>
     );
 }
 
