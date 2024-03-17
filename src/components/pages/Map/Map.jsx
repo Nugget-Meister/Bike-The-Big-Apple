5c11b392e4ed34d253748ef559d76bc28eff476c
Nugget-Meister <tremainecamacho@gmail.com>
2024-03-17 15:47:50 -0400

    state now refreshes when either address is input, fixes state resetting on each input

--- src/components/pages/Map/Map.jsx
+++ src/components/pages/Map/Map.jsx
@@ -1,5 +1,5 @@
 import './Map.css'
-import React from 'react';
+import React, { useEffect } from 'react';
 import MapWidget from './subcomponents/MapWidget';
 import {
     loadAPI, 
@@ -13,33 +13,51 @@ import { useState, createContext, useContext } from 'react';
 import { PathContext } from './helpers/pathContext.js';
 
 const Map = () => {
-
   const [path, setPath] = useState({
       start: {},
       destination: {}
     })
 
+  const [firstLoad, setfirstLoad] = useState(true)
+
+  useEffect(() => {
+    console.log(path, firstLoad)
+    if(!firstLoad){
+      console.log("Path updated... Reloading Queue")
+      loadQueue()
+    }
+  },[path])
+
   // const path = useContext(PathContext)
 
     const loadQueue = () => {
         // renderMap();
-        loadAutoComplete('start', 'start_details', 'Enter starting location.', setPath)
-        // loadAutoComplete('destination', 'destination_details', 'Enter starting location.', setPath)
+        loadAutoComplete('start', 'start_details', 'Enter starting location.', path, setPath)
+        loadAutoComplete('destination', 'destination_details', 'Enter starting location.', path, setPath)
 
     }
   
 
     const handleSubmit = (e) => {
       e.preventDefault()
-      console.log(e.target.start.value)
-      console.log(e.target.destination.value)
+      // console.log(path)
+      // console.log(e.target.start.value)
+      // console.log(e.target.destination.value)
+      console.log(path.start.geometry.location.lat())
+      console.log(path.start.geometry.location.lng())
+      // console.log(path.destination)
+      console.log(path.destination.geometry.location.lat())
+      console.log(path.destination.geometry.location.lng())
     }
    
 
     return (
       <>
         <PathContext.Provider value={{path, setPath}}>
-          <button onClick={() => loadAPI().then(() => loadQueue())}> Enable Queue</button>
+          <button onClick={() => loadAPI().then(() => {
+              loadQueue();
+              setfirstLoad(false);
+              })}> Enable Queue</button>
           <br />
           <form onSubmit={handleSubmit}>
             <SearchBox id='start'/>
