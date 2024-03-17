e9bdcb66c5a04c44516afe81746ab51f7ac8a2f1
Jalal Jonaid <jalaljonaid@Jalals-MacBook-Air.local>
2024-03-17 16:23:55 -0400

    Adds bite effect to button and additional styling

--- src/components/pages/Splash/Splash.jsx
+++ src/components/pages/Splash/Splash.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import './Splash.css'
 // import "../map"
 
 const Splash = () => {
     return (
-        <div className="bg-[#FCFFE7] flex items-center justify-center h-screen">
+        <div className="bg-[#FCFFE7] flex flex-col justify-start items-center pt-8 pb-8 min-h-screen">
           <div className="text-center">
             <div className="font-gotham text-5xl mb-20">
               <div>Bike The</div>
               <div>Big Apple</div>
             </div>
-            <img src='https://i.ibb.co/9sCPhwS/Bt-BA-Logo.png' alt="Logo" className="mx-auto mb-25" />
-            <button className="bg-black text-white px-6 py-2 rounded hover:bg-red-700 transition-colors">
+            <img src='https://i.ibb.co/9sCPhwS/Bt-BA-Logo.png' alt="BtBA_Logo" className="mx-auto mb-25 pb-12" />
+            <button className="bg-black text-white px-16 py-2 h-12 rounded-lg hover:bg-red-700 transition-colors bite-button">
               Start Biking!
             </button>
           </div>
