38a04cc9e2145628c7e03d21c7836f6e7fee2fb0
Jalal Jonaid <jalaljonaid@Jalals-MacBook-Air.local>
2024-03-17 15:58:43 -0400

    Adds splash page base

--- src/components/pages/Splash/Splash.jsx
+++ src/components/pages/Splash/Splash.jsx
@@ -3,8 +3,17 @@ import React from 'react';
 
 const Splash = () => {
     return (
-        <div className="container">
-          <h1 className='text-xl font-bold text-blue-400'>This is a test Tailwind Component</h1>
+        <div className="bg-[#FCFFE7] flex items-center justify-center h-screen">
+          <div className="text-center">
+            <div className="font-gotham text-5xl mb-20">
+              <div>Bike The</div>
+              <div>Big Apple</div>
+            </div>
+            <img src='https://i.ibb.co/9sCPhwS/Bt-BA-Logo.png' alt="Logo" className="mx-auto mb-25" />
+            <button className="bg-black text-white px-6 py-2 rounded hover:bg-red-700 transition-colors">
+              Start Biking!
+            </button>
+          </div>
         </div>
       );
 }
