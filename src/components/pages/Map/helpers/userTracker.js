a685b891fc08304a3ad26714fd2606d236646c8e
Nugget-Meister <tremainecamacho@gmail.com>
2024-03-24 12:09:53 -0400

    add retry attemps to getUserCoords(), needs export for pos data

--- src/components/pages/Map/helpers/userTracker.js
+++ src/components/pages/Map/helpers/userTracker.js
@@ -1,20 +1,52 @@
-const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-  };
-  
-  function success(pos) {
-    const crd = pos.coords;
-  
-    console.log("Your current position is:");
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-  }
-  
-  function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-  }
-  
-  navigator.geolocation.getCurrentPosition(success, error, options);
+
+
+const getUserCoords = () => {
+
+    let tries = 0;
+
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      };
+      
+    function success(pos) {
+        const crd = pos.coords;
+        
+        console.log("Your current position is:");
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+        return crd
+    }
+      
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`, 'Request timed out.');
+        tries++
+        console.log(tries, "times tried")
+        if (tries <= 3) {
+            performGet()
+        } else {
+            console.error("Max tries exceeded from timeout.")
+        }
+    }
+
+   const performGet = () => {
+        if("geolocation" in navigator){
+            navigator.geolocation.getCurrentPosition(success, error, options)
+        } else {
+            // Prompt user
+            console.error("Geolocation not detected or available on machine.")
+        }
+
+   }
+
+   performGet()
+      
+}
+
+export {
+    getUserCoords
+}
+
+
