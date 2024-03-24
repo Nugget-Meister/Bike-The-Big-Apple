cb4d8326a93c8e4c9124bd2f4a285aea93ea368c
Nugget-Meister <tremainecamacho@gmail.com>
2024-03-24 12:52:34 -0400

    exposed geolocation coords for use outside of fn. works like a promise

--- src/components/pages/Map/helpers/userTracker.js
+++ src/components/pages/Map/helpers/userTracker.js
@@ -1,6 +1,6 @@
 
 
-const getUserCoords = () => {
+const getUserCoords = async () => {
 
     let tries = 0;
 
@@ -9,39 +9,48 @@ const getUserCoords = () => {
         timeout: 5000,
         maximumAge: 0,
       };
+
       
-    function success(pos) {
-        const crd = pos.coords;
-        
-        console.log("Your current position is:");
-        console.log(`Latitude : ${crd.latitude}`);
-        console.log(`Longitude: ${crd.longitude}`);
-        console.log(`More or less ${crd.accuracy} meters.`);
-        return crd
-    }
+      const promise = new Promise((resolve, reject)=>{
+          function success(pos) {
+              const crd = pos.coords;
+              console.log("success.")
+            //   console.log("Your current position is:");
+            //   console.log(`Latitude : ${crd.latitude}`);
+            //   console.log(`Longitude: ${crd.longitude}`);
+            //   console.log(`More or less ${crd.accuracy} meters.`);
+              resolve(crd)
+          }
+            
+          function error(err) {
+              console.warn(`ERROR(${err.code}): ${err.message}`, 'Request timed out.');
+              tries++
+              console.log(tries, "times tried")
+              if (tries <= 3) {
+                  performGet()
+              } else {
+                  console.error("Max tries exceeded from timeout.")
+                  resolve(404)
+              }
+          }
       
-    function error(err) {
-        console.warn(`ERROR(${err.code}): ${err.message}`, 'Request timed out.');
-        tries++
-        console.log(tries, "times tried")
-        if (tries <= 3) {
+         const performGet = async () => {
+              if("geolocation" in navigator){
+                  navigator.geolocation.getCurrentPosition(success, error, options)
+              } else {
+                  console.error("Geolocation not detected or available on machine.")
+              }
+              
+         }
+
             performGet()
-        } else {
-            console.error("Max tries exceeded from timeout.")
-        }
-    }
+ 
+    })
 
-   const performGet = () => {
-        if("geolocation" in navigator){
-            navigator.geolocation.getCurrentPosition(success, error, options)
-        } else {
-            // Prompt user
-            console.error("Geolocation not detected or available on machine.")
-        }
+   return promise.then((res) => {
+    return res
+   })
 
-   }
-
-   performGet()
       
 }
 
