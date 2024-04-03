531ed4f96c984a8a0c036b7f2e40a1a725f39e3c
DaviiJones <davonbridgett@pursuit.org>
2024-04-03 18:28:37 -0400

    profile page almost done

--- src/components/pages/Profile/Profile.jsx
+++ src/components/pages/Profile/Profile.jsx
@@ -8,9 +8,9 @@ import "./Profile.css";
 const Profile = () => {
   const userData = useAuthContext;
   return (
-    <div className="min-h-screen bg-blue-500 flex flex-col items-center px-4 py-8">
+    <div className="min-h-screen bg-bike-blue flex flex-col items-center px-4 py-8">
       <img
-        src="https://unsplash.com/photos/man-in-blue-tank-top-riding-on-bicycle-during-daytime-spjyZ9btsSE"
+        src="https://images.unsplash.com/photo-1593057902358-68cfcd1d4872?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="profile"
         className="w-48 h-48 object-cover rounded-full md:w-64 md:h-64 border-4 border-white"
       />
@@ -28,7 +28,10 @@ const Profile = () => {
           <h2 className="text-2xl font-semibold text-white">Saved Routes</h2>
           <div className="flex items-center">
             <p className="text-2xl text-white">(3)</p>
-            <svg /*ADD ICON HERE!!!-JJ*/ className="ml-2 w-6 h-6 text-gray-500" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+</svg>
+
           </div>
         </div>
       </div>
@@ -41,21 +44,30 @@ const Profile = () => {
 
       <div className="grid grid-cols-2 gap-4 mt-4">
         {Array.from({ length: 4 }).map((_, index) => (
-          <div key={index} className="p-8 bg-white rounded-lg shadow-md">
-            <h3 className="text-md font-semibold">Header {index + 1}</h3>
-            <p className="text-sm text-gray-500">Subheader {index + 1}</p>
+          <div key={index} className="p-8 bg-bike-off-white rounded-lg shadow-md">
+            <h3 className="text-md font-semibold">Achievement Header {index + 1}</h3>
+            <p className="text-sm text-gray-500">Achievement {index + 1}</p>
           </div>
         ))}
       </div>
 
       <div className="w-11/12 h-1 bg-white mt-5"></div>
-
+      <h2 className="self-start text-lg font-semibold mt-5 text-white">
+      Acheivements
+      </h2>    
       <div className="flex mt-5">
         {Array.from({ length: 5 }).map((_, index) => (
           <svg
-            /*ADD STAR SVG HERE!!-JJ*/ key={index}
-            className="w-6 h-6 text-yellow-500"
-          />
+          width="24"
+          height="24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <use href="Bike-The-Big-Apple/src/components/assets/shield.svg" />
+        </svg>
         ))}
       </div>
     </div>
