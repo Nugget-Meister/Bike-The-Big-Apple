d74c025eea3fe87f19f63561cccb0246c436ead6
Nugget-Meister <tremainecamacho@gmail.com>
2024-04-21 01:32:17 -0400

    profile button now animates to next page

--- src/components/pages/Profile/ProfileButton.jsx
+++ src/components/pages/Profile/ProfileButton.jsx
@@ -1,29 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuthContext } from "../../../Providers/AuthProvider";
+import defaultUserIcon from '../../../../public/user.svg'
+import LoadingScreen from "../../common/loadingScreen";
+
 
 const ProfileButton = () => {
 
+  const navigate = useNavigate()
   const userData = useAuthContext()
+  const [isAnimating, setIsAnimating] = useState(false)
+
+  const animateToProfile = async (duration) => {
+    setIsAnimating(true)
+    setTimeout(() => {
+      navigate('/profile')
+    }, duration)
+  }
 
 
   return (
     <div className="z-10 ProfileButton absolute bottom-0 right-0 bg-bike-blue text-white p-2 m-2 my-4 rounded-full w-[4rem] h-[4rem]">
-      <Link to="/profile">
-        {/* <Button
-          style={{
-            backgroundColor: "black",
-            color: "white",
-            borderRadius: "15px",
-          }}
-        > */}
-        <img
-        src={userData?.photoURL}
+      {/* <Link to="/profile"> */}
+      <img
+        src={userData?.photoURL || defaultUserIcon}
         alt="profile"
-        className="object-cover rounded-full md:w-64 md:h-64"
+        className="object-cover w-full rounded-full md:w-64 md:h-64"
+        onClick={()=> {animateToProfile(1000)}}
       />
         {/* </Button> */}
-      </Link>
+      {/* </Link> */}
+      {isAnimating ? (
+      <>
+        <LoadingScreen animationName={'expandFromCircle'}/>
+      </>) : null}
     </div>
   );
 };
