a12260dd11a2a2e316b3addec5dfd911e67d94c8
Nugget-Meister <tremainecamacho@gmail.com>
2024-04-21 16:45:36 -0400

    add slash

--- src/components/apicalls.js
+++ src/components/apicalls.js
@@ -2,8 +2,8 @@ const URL = import.meta.env.VITE_API_URL || import.meta.env.VITE_API_LOCAL
 
 console.log(URL)
 const getUserInfo = (email) => {
-    console.log(`${URL}userStatistics/email/${email}`)
-    return fetch(`${URL}userStatistics/email/${email}`)
+    console.log(`${URL}/userStatistics/email/${email}`)
+    return fetch(`${URL}/userStatistics/email/${email}`)
     .then(res => res.json())
     .catch(err => console.err(err))
 }
